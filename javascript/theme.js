0bbb79dc4d8ce9bce84615648c4883e692fd5ba5
EllenSirks <ellensirks1995@gmail.com>
2024-02-14 17:44:28 +1100

    test

--- javascript/theme.js
+++ javascript/theme.js
@@ -1,6 +1,6 @@
 function pic() {
-    var bgm = ['./images/pretty_antennae.PNG', './images/pretty_tarantula.PNG', './images/pretty_m83.PNG'];
-    // var bgm = ['https://github.com/EllenSirks/EllenSirks.github.io/tree/main/images/pretty_antennae.PNG','http://i.imgur.com/AOkCD.png', 'https://images2.alphacoders.com/599/599756.jpg'];
+    // var bgm = ['./images/pretty_antennae.PNG', './images/pretty_tarantula.PNG', './images/pretty_m83.PNG'];
+    var bgm = ['https://i.imgur.com/h8f386C.png', 'http://i.imgur.com/AOkCD.png', 'https://images2.alphacoders.com/599/599756.jpg'];
 
     index = Math.floor(Math.random() * bgm.length)
 
