74e018d481846b8965fc050d3e70079fcc2ba209
EllenSirks <ellensirks1995@gmail.com>
2024-02-14 18:24:41 +1100

    test

--- javascript/theme.js
+++ javascript/theme.js
@@ -1,37 +1,37 @@
-// function pic() {
-//     var bgm = ['images/pretty_antennae.PNG', 'images/pretty_tarantula.PNG', 'images/pretty_m83.PNG'];
+function pic() {
+    var bgm = ['images/pretty_antennae.PNG', 'images/pretty_tarantula.PNG', 'images/pretty_m83.PNG'];
 
-//     index = Math.floor(Math.random() * bgm.length)
+    index = Math.floor(Math.random() * bgm.length)
 
-//     $('.random_bg').css({
-//         'background' : 'url('+ bgm[index] + ') no-repeat',
-//         'background-position' : '100%',
-//         'background-size' : 'cover',
-//         'background-attachment': 'fixed',
-//         '-ms-overflow-style': 'none'
-//     });
+    // $('.random_bg').css({
+    //     'background' : 'url('+ bgm[index] + ') no-repeat',
+    //     'background-position' : '100%',
+    //     'background-size' : 'cover',
+    //     'background-attachment': 'fixed',
+    //     '-ms-overflow-style': 'none'
+    // });
 
-//   return index
+  return index
 
-// }
+}
 
-// var index = pic();
+var index = pic();
 
-// var credit = ["The Antennae galaxies", "The Tarantula Nebula", "Southern Pinwheel Galaxy (M83)"][index];
-
-// const para = document.createElement("p");
-// const node = document.createTextNode(credit);
-// para.setAttribute("style", "color:white; position:fixed; right:10px; bottom: 20px; font-weight:bold");
-// para.appendChild(node);
-// const element = document.getElementById("image_credit");
-// element.appendChild(para);
-
-var credit = ["The Antennae galaxies", "The Tarantula Nebula", "Southern Pinwheel Galaxy (M83)"];
-var index = Math.floor(Math.random() * credit.length)
+var credit = ["The Antennae galaxies", "The Tarantula Nebula", "Southern Pinwheel Galaxy (M83)"][index];
 
 const para = document.createElement("p");
-const node = document.createTextNode(credit[index]);
+const node = document.createTextNode(credit);
 para.setAttribute("style", "color:white; position:fixed; right:10px; bottom: 20px; font-weight:bold");
 para.appendChild(node);
 const element = document.getElementById("image_credit");
 element.appendChild(para);
+
+// var credit = ["The Antennae galaxies", "The Tarantula Nebula", "Southern Pinwheel Galaxy (M83)"];
+// var index = Math.floor(Math.random() * credit.length)
+
+// const para = document.createElement("p");
+// const node = document.createTextNode(credit[index]);
+// para.setAttribute("style", "color:white; position:fixed; right:10px; bottom: 20px; font-weight:bold");
+// para.appendChild(node);
+// const element = document.getElementById("image_credit");
+// element.appendChild(para);
