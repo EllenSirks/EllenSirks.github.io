76062831419ffd08daea79bec1473ff79ca2c77d
EllenSirks <ellensirks1995@gmail.com>
2024-02-16 15:34:01 +1100

    update

--- javascript/theme_nl.js
+++ javascript/theme_nl.js
@@ -25,3 +25,18 @@ para.setAttribute("style", "color:white; position:fixed; right:10px; bottom: 20p
 para.appendChild(node);
 const element = document.getElementById("image_credit_nl");
 element.appendChild(para);
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
