de956c80146d013dae865dfc4d8557a57280b59c
EllenSirks <ellensirks1995@gmail.com>
2024-02-20 21:46:15 +1100

    update

---
+++ javascript/bkg.js
@@ -0,0 +1,27 @@
+function pic() {
+    var bgm = ['images/pretty_antennae.PNG', 'images/pretty_tarantula.PNG', 'images/pretty_m83.PNG'];
+
+    index = Math.floor(Math.random() * bgm.length)
+
+    $('.random_bg').css({
+        'background' : 'url('+ bgm[index] + ') no-repeat',
+        'background-position' : '100%',
+        'background-size' : 'cover',
+        'background-attachment': 'fixed',
+        '-ms-overflow-style': 'none'
+    });
+
+  return index
+
+}
+
+var index = pic();
+
+var credit = ["The Antennae galaxies", "The Tarantula Nebula", "Southern Pinwheel Galaxy (M83)"][index];
+
+const para = document.createElement("p");
+const node = document.createTextNode(credit);
+para.setAttribute("style", "color:white; position:fixed; right:10px; bottom: 20px; font-weight:bold;");
+para.appendChild(node);
+const element = document.getElementById("image_credit");
+element.appendChild(para);
--- javascript/theme.js
+++ javascript/theme.js
@@ -1,31 +1,3 @@
-function pic() {
-    var bgm = ['images/pretty_antennae.PNG', 'images/pretty_tarantula.PNG', 'images/pretty_m83.PNG'];
-
-    index = Math.floor(Math.random() * bgm.length)
-
-    $('.random_bg').css({
-        'background' : 'url('+ bgm[index] + ') no-repeat',
-        'background-position' : '100%',
-        'background-size' : 'cover',
-        'background-attachment': 'fixed',
-        '-ms-overflow-style': 'none'
-    });
-
-  return index
-
-}
-
-var index = pic();
-
-var credit = ["The Antennae galaxies", "The Tarantula Nebula", "Southern Pinwheel Galaxy (M83)"][index];
-
-const para = document.createElement("p");
-const node = document.createTextNode(credit);
-para.setAttribute("style", "color:white; position:fixed; right:10px; bottom: 20px; font-weight:bold;");
-para.appendChild(node);
-const element = document.getElementById("image_credit");
-element.appendChild(para);
-
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -40,3 +12,11 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+// function getButtonCount() {
+//     return document.getElementsByTagName('button').length;
+// }
+
+// var buttons = getButtonCount();
+
+// console.log(buttons);
